2eae41e3fd37de42b9b0eebdf2ebba569adc8d88
Bruce Zhang <bruce.zhang@macys.com>
2017-03-08 15:27:03 -0500

    got redis working

--- indigo.js
+++ indigo.js
@@ -5,43 +5,41 @@ var fs = require('fs');
 var redis = require("redis");
 
 var redis_option={
-    host:"pub-redis-13746.us-central1-1-1.gce.garantiadata.com",
-    port:13746
+    host:"pub-redis-14483.us-central1-1-1.gce.garantiadata.com",
+    port:14483
 };
 
 
 
 var client = redis.createClient(redis_option.port,redis_option.host,{no_ready_check: true});
 
-// client.auth('', function (err) {
-//     if (err) throw err;
-// });
-client.set("foo", "string val", redis.print);
-// if you'd like to select database 3, instead of 0 (default), call
-// client.select(3, function() { /* ... */ });
-client.get("foo", function (err, reply) {
+client.auth('brucezhang123', function (err) {
     if (err) throw err;
-    console.log(reply.toString());
 });
+// client.set("foo", "string val", redis.print);
+// // if you'd like to select database 3, instead of 0 (default), call
+// // client.select(3, function() { /* ... */ });
+// client.get("foo", function (err, reply) {
+//     if (err) throw err;
+//     console.log(reply.toString());
+// });
 client.on("error", function (err) {
     console.log("Error " + err);
 });
 
-
+client.get("imagedes", function (err, reply) {
+        if (err) throw err;
+        console.log(reply.toString().split(',').map(m=>parseFloat(m)));
+    });
 indico.apiKey =  '66df1bd4798a7a3603a61ea1f0c4f37a';
 
-var response = function(res) { console.log(res);
-    fs.writeFile("./imgDesc/desc.txt", res, function(err) {
-        if(err) {
-            return console.log(err);
-        }
+var response = function(res) { 
+    client.set("imagedes", res.toString(), redis.print);
 
-        console.log("The file was saved!");
-    });     
  }
 var logError = function(err) { console.log(err); }
   
-// // single example
+// single example
 // indico.imageFeatures("https://slimages.macysassets.com/is/image/MCY/products/5/optimized/2544415_fpx.tif?$filtersm$&wid=126&hei=154")
 //   .then(response)
 //   .catch(logError);
